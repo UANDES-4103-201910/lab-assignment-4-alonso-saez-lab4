814ab78758ccf39363cf86bdcb1edefda37e9304
AntoniaSaez <asaez1@miuandes.cl>
2019-04-04 23:56:05 -0300

    Arreglo ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,12 +1,13 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+  validate :start_date, :presence => true
   before_save :ticket_cannot_be_created_after_the_start_date
   before_save :no_ticket_could_be_bought_after_start_date
 
   def ticket_cannot_be_created_after_the_start_date
     start_date_of_the_event = TicketType.joins(:events).where(events: {event_id: self}.find(:start_date))
-    if self.created_at.present? && start_date_of_the_event < self.created_at
+    if start_date_of_the_event < Date.today
       errors.add(:start_date, 'the event cant start in the past than the creation date')
     end
   end

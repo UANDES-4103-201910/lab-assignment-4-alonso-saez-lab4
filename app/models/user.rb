8cdf7d01eb19b413a1848cd62f17b1983923ff40
AntoniaSaez <asaez1@miuandes.cl>
2019-04-04 22:21:59 -0300

    User corregido

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :orders
 
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  validates :phonenumber, length: {in: 9..12}, :message => 'Your phone number has less than 9 numbers'}
+  validates :phonenumber, length: { in: 9..12, :message => 'Your phone number has less than 9 numbers'}
   validates :password, length: {minimum: 8, :message => 'Your password has less than 8 characters'}
   validates :password, length: {maximum: 12, :messages => 'Your password has more than 12 characters'}
 
